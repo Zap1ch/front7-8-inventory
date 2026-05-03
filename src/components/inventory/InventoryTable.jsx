148674c9cd3b8038717ce4ae725284bd07203cc2
Taras frontendLab7 <tarasp41@gmail.com>
2026-05-03 15:34:36 +0300

    Додав стилізацію галереї та виправив таблицю інвентарю

--- src/components/inventory/InventoryTable.jsx
+++ src/components/inventory/InventoryTable.jsx
@@ -2,45 +2,49 @@ import { Link } from "react-router-dom";
 
 function InventoryTable({ inventory, onDeleteClick }) {
   return (
-    <table className="inventory-table">
-      <thead>
-        <tr>
-          <th>Фото</th>
-          <th>Назва</th>
-          <th>Опис</th>
-          <th>Дії</th>
-        </tr>
-      </thead>
-
-      <tbody>
-        {inventory.map((item) => (
-          <tr key={item.id}>
-            <td>
-              {item.photo_url ? (
-                <img
-                  src={`http://127.0.0.1:3000${item.photo_url}`}
-                  alt={item.name}
-                  className="preview-img"
-                />
-              ) : (
-                "—"
-              )}
-            </td>
-
-            <td>{item.name}</td>
-            <td>{item.description || "Без опису"}</td>
-
-            <td className="actions">
-              <Link to={`/inventory/${item.id}`}>Переглянути</Link>
-              <Link to={`/inventory/${item.id}/edit`}>Редагувати</Link>
-              <button onClick={() => onDeleteClick(item)}>
-                Видалити
-              </button>
-            </td>
+    <div className="table-wrapper">
+      <table className="inventory-table">
+        <thead>
+          <tr>
+            <th>Фото</th>
+            <th>Назва</th>
+            <th>Опис</th>
+            <th>Дії</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+
+        <tbody>
+          {inventory.map((item) => (
+            <tr key={item.id}>
+              <td>
+                {item.photo_url ? (
+                  <img
+                    src={`http://127.0.0.1:3000${item.photo_url}`}
+                    alt={item.name}
+                    className="preview-img"
+                  />
+                ) : (
+                  "—"
+                )}
+              </td>
+
+              <td>{item.name}</td>
+              <td>{item.description || "Без опису"}</td>
+
+              <td>
+                <div className="actions">
+                  <Link to={`/inventory/${item.id}`}>Переглянути</Link>
+                  <Link to={`/inventory/${item.id}/edit`}>Редагувати</Link>
+                  <button onClick={() => onDeleteClick(item)}>
+                    Видалити
+                  </button>
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
